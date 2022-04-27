79bbc05f63596bdaaf81fe6b5e7a4cb20505ddd8
Janiece Eleonor <janiecee@ev-h3.kzn.21-school.ru>
2022-04-27 17:52:00 +0300

    janiecee strcspn

--- src/s21_string.h
+++ src/s21_string.h
@@ -6,3 +6,4 @@ void *s21_memset(void *str, int c, size_t n);
 char *s21_strcpy(char *dest, const char *src);
 size_t s21_strlen(const char *str);
 char *s21_strncpy(char *dest, const char *src, size_t n);
+size_t s21_strcspn(const char *str1, const char *str2);
--- src/s21_strncpy.c
+++ src/s21_strncpy.c
@@ -1,17 +1,17 @@
-#include <string.h>
+// #include <string.h>
 #include "s21_string.h"
 
-int main(void) {
-    char dest[] = "ghjldfhkjgdkgjhfd\0";
-    char dest0[] = "ghjldfhkjgdkgjhfd\0";
-    char src[] = "^%^&*^&Y*&#IHJF\0";
-    size_t n = 5;
-    strncpy(dest, src, n);
-    puts(dest);
-    s21_strncpy(dest0, src, n);
-    puts(dest0);
-    return 0;
-}
+// int main(void) {
+//     char dest[] = "ghjldfhkjgdkgjhfd\0";
+//     char dest0[] = "ghjldfhkjgdkgjhfd\0";
+//     char src[] = "^%^&*^&Y*&#IHJF\0";
+//     size_t n = 5;
+//     strncpy(dest, src, n);
+//     puts(dest);
+//     s21_strncpy(dest0, src, n);
+//     puts(dest0);
+//     return 0;
+// }
 // Копирует до n символов из строки, на которую указывает src, в dest
 char *s21_strncpy(char *dest, const char *src, size_t n) {
     size_t i;
