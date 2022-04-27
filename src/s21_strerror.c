7ff923e107101d664c84a7a3171e61256c6701da
Janiece Eleonor <janiecee@ev-h3.kzn.21-school.ru>
2022-04-27 19:35:09 +0300

    strerror

---
+++ src/s21_strerror.c
@@ -0,0 +1,42 @@
+// #include <string.h>
+#include <errno.h>
+#include "s21_string.h"
+
+#define	UPREFIX	"Unknown error: "
+
+int main(void) {
+    FILE *fp=0;     // Определяем переменную указателя файла fp
+    // Открываем файл /tmp/book1.c как доступный только для чтения
+    if ( (fp=fopen("/tmp/book1.c","r")) == 0 )
+    {
+        printf("Не удалось открыть файл /tmp/book1.c (%d:%s). \n", errno, s21_strerror(errno)); 
+    }
+    
+    // закрываем файл
+    if ( fp!=0 ) fclose(fp);
+    return 0;
+}
+
+static char ebuf[40] = UPREFIX;
+
+char *s21_strerror(int errnum) {
+    extern const int sys_nerr;
+	extern const char *const sys_errlist[];
+	register char *p, *t;
+	char tmp[40];
+
+	if (errnum < sys_nerr)
+		return((char*)sys_errlist[errnum]);
+
+	/* Do this by hand, so we don't include stdio(3). */
+	t = tmp;
+	do {
+		*t++ = "0123456789"[errnum % 10];
+	} while (errnum /= 10);
+	for (p = ebuf + sizeof(UPREFIX) - 1;;) {
+		*p++ = *--t;
+		if (t <= tmp)
+			break;
+	}
+	return(ebuf);
+}
--- src/s21_string.h
+++ src/s21_string.h
@@ -7,3 +7,4 @@ char *s21_strcpy(char *dest, const char *src);
 size_t s21_strlen(const char *str);
 char *s21_strncpy(char *dest, const char *src, size_t n);
 size_t s21_strcspn(const char *str1, const char *str2);
+char *s21_strerror(int errnum);
