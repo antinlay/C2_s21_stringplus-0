d41553ccc4b2c546b88780cdf730b270551113c6
Janiece Eleonor <janiecee@ev-j5.kzn.21-school.ru>
2022-05-07 17:53:25 +0300

    janiecee MINUS  flag fix

--- src/sprintf.c
+++ src/sprintf.c
@@ -14,9 +14,19 @@ struct s_sprintf {
     int len;       //  poluchennaya dlina stroki
     int sign;      //  (+ ili -) polozhitel'noe ili otricatel'noe
     int perc;      // % procent
-    int sp;        // ' ' flag probela
+    int space;        // ' ' flag probela
     char spec;     // specificator (d, c, s, i)
 } p;
+
+int s21_atoi(char *ibuf, int i) {
+    int digit = 0;
+    for (; ibuf[i] >= 0x30 && ibuf[i] <= 0x39; i++) {
+        digit = digit + (ibuf[i] & 0x0F);
+        digit *= BASE;
+    }
+    digit /= BASE;
+    return digit;
+}
 
 // void sign_itoa(int symb, char *str) {
 //     char buf[50] = "";
@@ -44,7 +54,7 @@ void p_flag(char input_char) {
     if (input_char != 'f') p.spec += 'f';
     if (input_char != 'u') p.spec += 'u';
     if (input_char != 's') p.spec += 's';
-    if (input_char != '-') p.sp = 0;
+    if (input_char != '-') p.space = 0;
     if (input_char != '+') p.sign = 0;
     if (input_char != '%') p.perc = 0;
 }
@@ -59,7 +69,7 @@ void parser(char *str, const char *format) {
     p.len = 0;
     p.sign = 0;
     p.perc = 0;
-    p.sp = 0;
+    p.space = 0;
     int i = 0, j = 0, symb = 0, digit = 0, tmp = 0, sp = 0;
     char buf[40];
     while (format[i] != '\0') {
@@ -122,36 +132,42 @@ void parser(char *str, const char *format) {
             // if (*++format = 'u') {
             //     *str++ = va_arg(p.args, float);
             // }
+            // CHECK '-' and FLAG '+'
             if (format[i + 1] == '-' && p.sign != '+') {
                 p.min = '-';
                 i++;
-                while (format[i] == '-' || format[i] == '+') {
+                // NOT INCLUDE ADDITINAL '-' or '+'
+                while (format[i] == '-') {
                     p_flag(format[i]);
                     i += 1;
                 }
-                while (format[i] >= 0x30 && format[i] <= 0x39) {
-                    // s21_size_t k = 0;
+                // ATOI
+                for (; format[i] >= 0x30 && format[i] <= 0x39;) {
                     digit = digit + (format[i] & 0x0F);
                     digit *= BASE;
-                    // buf[k] = format[i];
-                    i++;
+                    printf("%c\n%d\n", format[i], digit);
+                    i += 1;
                 }
                 digit /= BASE;
-                tmp = digit;
-                printf("tmp %d\n", tmp);
-                while (tmp > 0) {
+                // tmp = digit;
+                // symb = va_arg(p.args, int);
+                printf("symb %d\n", symb);
+                // SPACE COUNT
+                while (symb > 0) {
                     sp += 1;
-                    tmp /= BASE;
+                    symb /= BASE;
                     // for (int m = digit; m > 0; m--) {
                     //     sp += 1;
-                        printf("sp %d\n", sp);
+                    printf("sp %d\n", sp);
                     //     // j--;
                     // }
                 }
-                p.sp = digit - sp;
+                p.space = digit - sp;
                 sp = digit - sp;
-                printf("p.sp %d\n", p.sp);
-                if (format[i] == 'd' && p.sp == 0) {
+                printf("p.space %d\n", p.space);
+                // FIND SPEC 'd'
+                if (format[i] == 'd' && p.space == 0) {
+                    p.spec = 'd';
                     s21_size_t k = 0;
                     symb = va_arg(p.args, int);
                     s21_itoa(symb, BASE, buf);
@@ -162,9 +178,10 @@ void parser(char *str, const char *format) {
                         k++;
                     } while (k < s21_strlen(buf));
                 }
-                if (format[i] == 'd' && p.sp != 0) {
+                // FIND SPEC 'd' WITH SPACE
+                if (format[i] == 'd' && p.space != 0) {
                     s21_size_t k = 0;
-                    // int sp = p.sp;
+                    // int sp = p.space;
                     symb = va_arg(p.args, int);
                     s21_itoa(symb, BASE, buf);
                     printf("buf sp %s\n", buf);
@@ -175,10 +192,11 @@ void parser(char *str, const char *format) {
                     } while (k < s21_strlen(buf));
                     while (sp--) {
                         str[j++] = ' ';
-                        // printf("str %s\np.sp %d\n", str, sp);
+                        // printf("str %s\np.space %d\n", str, sp);
                     }
                 }
             }
+            // INIT NULL ALL STRUCT
             // struct s_sprintf init;
             p.wdt = 0;
             p.prc = 0;
@@ -188,12 +206,12 @@ void parser(char *str, const char *format) {
             p.len = 0;
             p.sign = 0;
             p.perc = 0;
-            p.sp = 0;
+            p.space = 0;
             // if (++*format == '.') {
             //     p.pnt = 1;
             // }
             // if (++*format == ' ') {
-            //     p.sp = 1;
+            //     p.space = 1;
             // }
         } else {
             str[j] = format[i];
@@ -214,26 +232,9 @@ int s21_sprintf(char *str, const char *format, ...) {
 int main(void) {
     char str0[100], str1[100];
     int num0 = 19;
-    int num1 = 149;
+    int num1 = -149;
     char test = 'Q';
-    // struct s_sprintf p;
-    // p.wdt = 0;
-    // p.prc = 0;
-    // p.zero = 0;
-    // p.pnt = 0;
-    // p.min = 0;
-    // p.len = 0;
-    // p.sign = 0;
-    // p.perc = 0;
-    // p.sp = 0;
-    // t_print *p;
-    // char *l = "1231";
-    // struct s_sprintf init;
-    // struct p;
-    // printf("%lu\n", strcspn(str1, "-1234567890"));
-    // printf("%lu\n", strspn("1616gdfg", "-1234567890"));
-    // parser(str, format, init);
-    char format[50] = "CHAR: %-13d INT: %+dvgfd %c";
+    char format[50] = "CHAR: %-6d INT: %+dvgfd %c";
     s21_sprintf(str0, format, num0, num1, test);
     sprintf(str1, format, num0, num1, test);
     printf("STR0: %s\nSTR1: %s\nCHAR: %c\n", str0, str1, test);
@@ -244,6 +245,6 @@ int main(void) {
     //     "%d\nperc = %d\nsp = %d\n",
     //     p.wdt, p.prc, p.zero, p.pnt,
     //     p.min, p.len, p.sign, p.perc,
-    //     p.sp);
+    //     p.space);
     return 0;
 }
